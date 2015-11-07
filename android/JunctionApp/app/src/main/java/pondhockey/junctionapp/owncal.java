c16152e25cc9ae83f5fff5b6937a8bd641c144ae
sakkoja <joellehtela@gmail.com>
2015-11-07 13:55:17 +0200

    värkkäilyjä

---
+++ android/JunctionApp/app/src/main/java/pondhockey/junctionapp/owncal.java
@@ -0,0 +1,13 @@
+package pondhockey.junctionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class owncal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owncal);
+    }
+}
