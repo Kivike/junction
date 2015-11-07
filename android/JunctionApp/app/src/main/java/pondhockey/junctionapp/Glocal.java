8fe45aef8910ca8b61b322ef44e499beb3ed843c
Roope Rajala <r_rajala@yahoo.com>
2015-11-07 18:39:54 +0200

    Event list working in global calendar view

--- android/JunctionApp/app/src/main/java/pondhockey/junctionapp/Glocal.java
+++ android/JunctionApp/app/src/main/java/pondhockey/junctionapp/Glocal.java
@@ -26,9 +26,13 @@ public class Glocal extends ListActivity {
         setContentView(R.layout.activity_glocal);
 
         events = new ArrayList<>();
-        events.add("Footbal");
-        events.add("Basketball");
-        events.add("Tennis");
+        events.add("Football 17:00 - 18:00");
+        events.add("Basketball 18:30 - 19:30");
+        events.add("Tennis 19:15 - 20:30");
+
+        eventList = (ListView) findViewById(R.id.eventListView);
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, events);
+        eventList.setAdapter(listAdapter);
 
         Button addEventButton = (Button) findViewById(R.id.addEventButton);
         addEventButton.setOnClickListener(new View.OnClickListener() {
@@ -42,15 +46,14 @@ public class Glocal extends ListActivity {
         calendar.setShownWeekCount(2);
         calendar.setFirstDayOfWeek(Calendar.MONDAY);
 
-        //listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, events);
-        //setListAdapter(listAdapter);
-
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 updateEventList(year, month, dayOfMonth);
             }
         });
+
+
     }
 
     private void getAllEvents() {
